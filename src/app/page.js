281372adaf43da6f873c73f18b3ac05a22407a3b
John Doe <get.in.touch.with.me@skiff.com>
2025-02-23 23:07:01 +0200

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div >
-         <h1>Auth project with YT Sahand</h1>
+         <h1> HOME page, Auth project with YT Sahand</h1>
     </div>
   );
 }
---
+++ src/app/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+import { SignUp } from '@clerk/nextjs';
+
+export default function SignUpPage() {
+  return (
+    <div className='sign-up-page'>
+      <SignUp />
+    </div>
+  );
+}
