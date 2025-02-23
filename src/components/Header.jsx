554108c3400c8530b6747b68134d4536696114bf
Nextjs <tech.united.it@gmail.com>
2025-02-24 00:16:30 +0200

    from span to div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,9 +25,9 @@ export default function Header() {
         {/* logo */}
         <Link href='/' className='text-2xl font-extrabold group cursor-pointer'>
 
-          <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>
+          <div className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>
             Auth
-          </span>
+          </div>
           <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>
             App
           </span>
