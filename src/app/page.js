e9c72ffd089321658bf172e4983b219038a2e814
Nextjs <tech.united.it@gmail.com>
2025-02-24 22:53:27 +0200

    home pahe MY MY

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div >
-         <h1> HOME page, Auth project with YT Sahand</h1>
+         <h1> 777 MY MY HOME page, Auth project with YT Sahand</h1>
          <h2>h2 tag added for testing</h2>
          <h3>h3 testing</h3>
          <h4>h4 testing h4</h4>
