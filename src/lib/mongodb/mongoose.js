bfe2b1d5cb8c3fad833efb0cf1f23b42b98bfed8
Nextjs <tech.united.it@gmail.com>
2025-02-25 19:53:12 +0200

    url changed

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -15,7 +15,7 @@ export const connect = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URL, {
       dbName: 'next-auth-app',
       useNewUrlParser: true,
       useUnifiedTopology: true,
