466f2eda4b1f75d5cc1e734cf14c0a24b8c6ec9b
Nextjs <tech.united.it@gmail.com>
2025-02-26 08:21:30 +0200

    home page changes

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
 
   return (
     <div >
-         <h1> 777 MY MY HOME page, Auth project with YT Sahand</h1>
+         <h1> 88888 MY MY HOME page, Auth project with YT Sahand</h1>
          <h2>h2 tag added for testing</h2>
          <h3>h3 testing</h3>
          <h4>h4 testing h4</h4>
