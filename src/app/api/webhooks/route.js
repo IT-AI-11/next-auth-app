dba8b5dabcb88bd6ac8f14be1f1a279b93531c52
Nextjs <tech.united.it@gmail.com>
2025-02-26 12:41:05 +0200

    webhook updated 444

--- src/app/api/webhooks/route.js
+++ src/app/api/webhooks/route.js
@@ -237,11 +237,11 @@ export async function POST(req) {
   const headerPayload = await headers()
   const svix_id = headerPayload.get('svix-id')
   const svix_timestamp = headerPayload.get('svix-timestamp')
-  //const svix_signature = headerPayload.get('svix-signature') original
+  const svix_signature = headerPayload.get('svix-signature') 
 
   // If there are no headers, error out
-  //if (!svix_id || !svix_timestamp || !svix_signature) { original
-  if (!svix_id || !svix_timestamp) {
+  if (!svix_id || !svix_timestamp || !svix_signature) { 
+  //if (!svix_id || !svix_timestamp) {
     return new Response('Error: Missing Svix headers', {
       status: 400,
     })
@@ -260,7 +260,7 @@ export async function POST(req) {
     evt = wh.verify(body, {
       'svix-id': svix_id,
       'svix-timestamp': svix_timestamp,
-      //'svix-signature': svix_signature, original
+      'svix-signature': svix_signature,
     }) 
     //console.log(evt)
 
